923fd2948c4bdcf705e33e23b5110b84d6dcf96f
Arpitduhoon <151802946+Arpitduhoon@users.noreply.github.com>
2024-12-25 19:14:23 +0530

    added a list named s

---
+++ frist.py
@@ -0,0 +1,20 @@
+
+#str = "hello boy how are you"
+#for i in range(len(str)):
+ #   print(str[i])
+#print(str[0:10])
+#print(str.replace("l","s"))
+#print(str.split(" "))
+#s = str.split(" ")
+#print( "after spliting " , s)
+#for i in s:
+#    print(i)
+#s[1:3] = ["raju ","kuvara"]
+#print("after replacing ",s)
+
+a = [1,4,2,3,1,0,6,7,8,9,0]
+print(a)
+a.sort()
+print(a)
+a.extend(a)
+print(a)
